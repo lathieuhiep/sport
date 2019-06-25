7858b4c96901a3adc3ffe5dd6437550974d60bf9
KhacDiep <khacdiepkma90@gmail.com>
2019-06-25 09:34:57 +0700

    add option select cat

--- extension/elementor/widgets/post-grid.php
+++ extension/elementor/widgets/post-grid.php
@@ -95,13 +95,41 @@ class sport_post_grid extends Widget_Base {
         );
 
         $this->add_control(
-            'input_id_post',
+            'select_cat',
             [
-                'label'         =>  esc_html__( 'Input Id Post', 'sport' ),
-                'type'          =>  Controls_Manager::TEXT,
-                'default'       =>  '',
+                'label'         =>  esc_html__( 'Select Category', 'sport' ),
+                'type'          =>  Controls_Manager::SELECT2,
+                'options'       =>  sport_check_get_cat( 'category' ),
+                'multiple'      =>  true,
                 'label_block'   =>  true,
-                'description'   =>  esc_html__( 'Ex input id post: 1,2', 'sport' ),
+            ]
+        );
+
+        $this->add_control(
+            'limit',
+            [
+                'label'     =>  esc_html__( 'Number of Posts', 'sport' ),
+                'type'      =>  Controls_Manager::NUMBER,
+                'default'   =>  4,
+                'min'       =>  1,
+                'max'       =>  100,
+                'step'      =>  1,
+            ]
+        );
+
+        $this->add_control(
+            'order_by',
+            [
+                'label'     =>  esc_html__( 'Order By', 'sport' ),
+                'type'      =>  Controls_Manager::SELECT,
+                'default'   =>  'id',
+                'options'   =>  [
+                    'id'            =>  esc_html__( 'Post ID', 'sport' ),
+                    'author'        =>  esc_html__( 'Post Author', 'sport' ),
+                    'title'         =>  esc_html__( 'Title', 'sport' ),
+                    'date'          =>  esc_html__( 'Date', 'sport' ),
+                    'rand'          =>  esc_html__( 'Random', 'sport' ),
+                ],
             ]
         );
 
@@ -238,131 +266,144 @@ class sport_post_grid extends Widget_Base {
     protected function render() {
 
         $settings       =   $this->get_settings_for_display();
+        $cat_post       =   $settings['select_cat'];
+        $limit_post     =   $settings['limit'];
+        $order_by_post  =   $settings['order_by'];
         $order_post     =   $settings['order'];
-        $input_id_post  =   $settings['input_id_post'];
 
-        if ( !empty( $input_id_post ) ) :
-
-            $ids_post = explode( ",", $input_id_post  );
+        if ( !empty( $cat_post ) ) :
 
             $args = array(
                 'post_type'             =>  'post',
-                'post__in'              =>  $ids_post,
+                'posts_per_page'        =>  $limit_post,
+                'orderby'               =>  $order_by_post,
+                'order'                 =>  $order_post,
+                'cat'                   =>  $cat_post,
+                'ignore_sticky_posts'   =>  1,
+            );
+
+        else:
+
+            $args = array(
+                'post_type'             =>  'post',
+                'posts_per_page'        =>  $limit_post,
+                'orderby'               =>  $order_by_post,
                 'order'                 =>  $order_post,
                 'ignore_sticky_posts'   =>  1,
             );
 
-            $query = new \ WP_Query( $args );
+        endif;
 
-            if ( $query->have_posts() ) :
+        $query = new \ WP_Query( $args );
 
-        ?>
+        if ( $query->have_posts() ) :
 
-            <div class="element-post-grid post-grid-masory">
-                <div class="row">
-                    <?php
-                    $i = $j = 0 ;
-                    while ( $query->have_posts() ): $query->the_post();
+    ?>
 
-                        $total_posts    =   $query->post_count;
+        <div class="element-post-grid post-grid-masory">
+            <div class="row">
+                <?php
+                $i = $j = 0 ;
+                while ( $query->have_posts() ): $query->the_post();
 
-                        if ( $i % 4 == 0 ) :
-                            $class_column = 6;
-                        else:
-                            $class_column = 3;
-                        endif;
+                    $total_posts    =   $query->post_count;
 
-                        if ( $i % 4 != 3 ) :
+                    if ( $i % 4 == 0 ) :
+                        $class_column = 6;
+                    else:
+                        $class_column = 3;
+                    endif;
 
-                    ?>
+                    if ( $i % 4 != 3 ) :
 
-                        <div class="col-12 col-sm-6 col-md-4 col-lg-<?php echo esc_attr( $class_column ); ?> item-col">
+                ?>
 
-                    <?php endif; ?>
+                    <div class="col-12 col-sm-6 col-md-4 col-lg-<?php echo esc_attr( $class_column ); ?> item-col">
 
-                            <div class="item-post">
-                                <div class="item-post__thumbnail">
-                                    <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-                                        <?php if ( has_post_thumbnail() ) : ?>
+                <?php endif; ?>
 
-                                            <img src="<?php echo esc_url( Group_Control_Image_Size::get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'thumbnail', $settings ) ); ?>" alt="<?php the_title(); ?>">
+                        <div class="item-post">
+                            <div class="item-post__thumbnail">
+                                <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+                                    <?php if ( has_post_thumbnail() ) : ?>
 
-                                        <?php else: ?>
+                                        <img src="<?php echo esc_url( Group_Control_Image_Size::get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'thumbnail', $settings ) ); ?>" alt="<?php the_title(); ?>">
 
-                                            <img src="<?php echo esc_url( get_theme_file_uri( '/images/no-image.png' ) ) ?>" alt="<?php the_title(); ?>" />
+                                    <?php else: ?>
 
-                                        <?php endif; ?>
-                                    </a>
-                                </div>
-
-                                <p class="item-post__date d-flex align-items-center justify-content-center flex-column">
-                                    <span class="day">
-                                        <?php echo esc_html( get_the_date( 'd' ) ); ?>
-                                    </span>
-
-                                    <span class="month">
-                                        <?php echo esc_html( get_the_date( 'Sm' ) ); ?>
-                                    </span>
-                                </p>
-
-                                <div class="item-content text-center">
-                                    <h3 class="item-post__title">
-                                        <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-                                            <?php echo esc_html( wp_trim_words( get_the_title(), $settings['title_post_length'] ) ); ?>
-                                        </a>
-                                    </h3>
-
-                                    <?php if ( $settings['show_excerpt'] == 1 && $i % 4 != 2 && $i % 4 != 3 ) : ?>
-
-                                        <p class="item-excerpt">
-                                            <?php
-                                            if ( has_excerpt() ) :
-                                                echo esc_html( wp_trim_words( get_the_excerpt(), $settings['excerpt_length'], '[...]' ) );
-                                            else:
-                                                echo esc_html( wp_trim_words( get_the_content(), $settings['excerpt_length'], '[...]' ) );
-                                            endif;
-                                            ?>
-                                        </p>
-
-                                    <?php
-                                    endif;
-
-                                    if ( $i % 4 == 0 ) :
-                                    ?>
-
-                                        <p class="item-post__comment">
-                                            <?php
-                                            comments_popup_link(
-                                                esc_html__( '0 Comment', 'cogito' ),
-                                                esc_html__( '1 Comment', 'cogito' ),
-                                                esc_html__( '% Comment(s)', 'cogito' )
-                                            );
-                                            ?>
-                                        </p>
+                                        <img src="<?php echo esc_url( get_theme_file_uri( '/images/no-image.png' ) ) ?>" alt="<?php the_title(); ?>" />
 
                                     <?php endif; ?>
-                                </div>
+                                </a>
                             </div>
 
-                    <?php if ( $i % 4 != 2 || $j == $total_posts ) : ?>
+                            <p class="item-post__date d-flex align-items-center justify-content-center flex-column">
+                                <span class="day">
+                                    <?php echo esc_html( get_the_date( 'd' ) ); ?>
+                                </span>
 
+                                <span class="month">
+                                    <?php echo esc_html( get_the_date( 'Sm' ) ); ?>
+                                </span>
+                            </p>
+
+                            <div class="item-content text-center">
+                                <h3 class="item-post__title">
+                                    <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+                                        <?php echo esc_html( wp_trim_words( get_the_title(), $settings['title_post_length'] ) ); ?>
+                                    </a>
+                                </h3>
+
+                                <?php if ( $settings['show_excerpt'] == 1 && $i % 4 != 2 && $i % 4 != 3 ) : ?>
+
+                                    <p class="item-excerpt">
+                                        <?php
+                                        if ( has_excerpt() ) :
+                                            echo esc_html( wp_trim_words( get_the_excerpt(), $settings['excerpt_length'], '[...]' ) );
+                                        else:
+                                            echo esc_html( wp_trim_words( get_the_content(), $settings['excerpt_length'], '[...]' ) );
+                                        endif;
+                                        ?>
+                                    </p>
+
+                                <?php
+                                endif;
+
+                                if ( $i % 4 == 0 ) :
+                                ?>
+
+                                    <p class="item-post__comment">
+                                        <?php
+                                        comments_popup_link(
+                                            esc_html__( '0 Comment', 'cogito' ),
+                                            esc_html__( '1 Comment', 'cogito' ),
+                                            esc_html__( '% Comment(s)', 'cogito' )
+                                        );
+                                        ?>
+                                    </p>
+
+                                <?php endif; ?>
+                            </div>
                         </div>
 
-                    <?php endif; ?>
+                <?php if ( $i % 4 != 2 || $j == $total_posts ) : ?>
 
-                    <?php
-                        $i++;
-                    endwhile;
-                    wp_reset_postdata();
-                    ?>
-                </div>
+                    </div>
+
+                <?php endif; ?>
+
+                <?php
+                    $i++;
+                endwhile;
+                wp_reset_postdata();
+                ?>
             </div>
+        </div>
 
-        <?php
-
-            endif;
+    <?php
 
         endif;
+
     }
 
     protected function _content_template() {}
